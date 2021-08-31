2968e7190f119bf0d4e6311f8c7b3f38244d6e07
Zahraa Zahir <ZahraaZahiirr@gmail.com>
2021-08-31 09:36:19 +0300

    init commit

---
+++ app/src/main/java/com/example/tokyoolympics/data/domain/Game.kt
@@ -0,0 +1,5 @@
+package com.example.tokyoolympics.data.domain
+
+data class Game (
+    var x: String,
+)
---
+++ app/src/main/java/com/example/tokyoolympics/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tokyoolympics.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tokyoolympics.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
