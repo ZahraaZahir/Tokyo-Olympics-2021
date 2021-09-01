d4d25341566e6ea810b74faddfb0a10976ff4ed1
Zahraa Zahir <ZahraaZahiirr@gmail.com>
2021-09-01 09:14:57 +0300

    create base activity class

---
+++ app/src/main/java/com/example/tokyoolympics/ui/BaseActivity.kt
@@ -0,0 +1,30 @@
+package com.example.tokyoolympics.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+import com.example.tokyoolympics.data.DataManger
+
+abstract class BaseActivity<VB : ViewBinding> : AppCompatActivity() {
+
+    abstract val bindingInflater: (LayoutInflater) -> VB
+
+    private var _binding: ViewBinding? = null
+
+    protected val binding
+        get() = _binding as VB?
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = bindingInflater(layoutInflater)
+        setContentView(requireNotNull(_binding).root)
+        setUp()
+        addCallBacks()
+
+    }
+
+    abstract fun setUp()
+
+    abstract fun addCallBacks()
+}
